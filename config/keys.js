bab7fcc3a3da4a527265d730f66cef7607dcdaa8
Godwin Nicholas <iamgodwin@gmail.com>
2019-12-21 17:31:22 +0100

    make it better

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MongoURI: "mongodb://localhost/bulkEmail1",
+    MongoURI: "mongodb+srv://Godwin:godwin222sm@cluster0-q4j31.mongodb.net/test?retryWrites=true&w=majority",
 };
