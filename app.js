a1376123e9c6e2fb305edb5756007fa29032e2c6
Godwin Nicholas <iamgodwin@gmail.com>
2019-12-21 17:34:14 +0100

    make it better

--- app.js
+++ app.js
@@ -34,29 +34,29 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./public'))
 
-// Express session
-app.use(
-    session({
-        secret: 'secret',
-        resave: true,
-        saveUninitialized: true
-    })
-);
+// // Express session
+// app.use(
+//     session({
+//         secret: 'secret',
+//         resave: true,
+//         saveUninitialized: true
+//     })
+// );
 
 // Passport middleware
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 // Connect flash
-app.use(flash());
+// app.use(flash());
 
-// Global variables
-app.use(function (req, res, next) {
-    res.locals.success_msg = req.flash('success_msg');
-    res.locals.error_msg = req.flash('error_msg');
-    res.locals.error = req.flash('error');
-    next();
-});
+// // Global variables
+// app.use(function (req, res, next) {
+//     res.locals.success_msg = req.flash('success_msg');
+//     res.locals.error_msg = req.flash('error_msg');
+//     res.locals.error = req.flash('error');
+//     next();
+// });
 
 
 // routes
