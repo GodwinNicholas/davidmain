0f52f20048d1db58e37a772c31308d6c620f263e
Godwin Nicholas <iamgodwin@gmail.com>
2019-12-24 13:42:00 +0100

    err

--- utils/emailType.js
+++ utils/emailType.js
@@ -1,5 +1,5 @@
 function emailType(address) {
-    const regex = /@[a-zA-z]+/gi;
+    const regex = /@[a-zA-z0-9]+/gi;
     const emailType = address.match(regex)[0].slice(1);
     return emailType;
 }
