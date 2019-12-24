970de02e1c285574065acbc1658480f688b616ad
Godwin Nicholas <iamgodwin@gmail.com>
2019-12-24 09:58:21 +0100

    hdhd

--- utils/temp.js
+++ utils/temp.js
@@ -7,11 +7,11 @@ function wrapMsg(msg) {
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+            <link href="https://fonts.googleapis.com/css?family=Merriweather|Roboto&display=swap" rel="stylesheet">
             <title>Email</title>
             <style>
             span{
-                font-family: 'Roboto', sans-serif;
+                font-family: 'Merriweather', serif;
                 font-size: 1rem;
             }
             </style>
@@ -23,8 +23,8 @@ function wrapMsg(msg) {
         <script>
                 setTimeout(()=> {
                     const eBody = document.querySelector("#msg1");
-                    eBody.innerText = "hello";
-                } ,2000);
+                    eBody.innerText = eBody.innerText.replace(/_/gi, " ");
+                });
         </script>
         </body>
         
